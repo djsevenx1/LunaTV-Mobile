bff9e01e2420914cf86cb54fedcbf28719a48084
djsevenx1 <438889797@qq.com>
2026-06-04 18:44:43 +0800

    fix: Simplified build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,3 @@
-import java.util.Properties
-import java.io.FileInputStream
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -10,7 +7,6 @@ plugins {
 android {
     namespace = "org.moontechlab.lunatv"
     compileSdk = 36
-    ndkVersion = "29.0.14033849"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,22 +23,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
-    }
-
-    val keystorePropertiesFile = rootProject.file("key.properties")
-    val hasSigningConfig = keystorePropertiesFile.exists()
-
-    signingConfigs {
-        if (hasSigningConfig) {
-            create("release") {
-                val properties = Properties()
-                properties.load(FileInputStream(keystorePropertiesFile))
-                storeFile = file(properties.getProperty("storeFile")!!)
-                storePassword = properties.getProperty("storePassword")
-                keyAlias = properties.getProperty("keyAlias")
-                keyPassword = properties.getProperty("keyPassword")
-            }
-        }
     }
 
     buildTypes {
