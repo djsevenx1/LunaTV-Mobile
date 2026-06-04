b067215294287a39a3732b25062ac0ac013df0f2
djsevenx1 <438889797@qq.com>
2026-06-05 06:37:26 +0800

    fix: Add pluginManagement with AGP 8.1.0 and Kotlin 1.9.0

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,10 +4,15 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.1.0" apply false
+        id("com.android.library") version "8.1.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
