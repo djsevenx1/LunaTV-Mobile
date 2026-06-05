1692b8a48ab6590165be15e80040d91092e5c096
djsevenx1 <438889797@qq.com>
2026-06-05 11:35:27 +0800

    fix: Use old-style Flutter Gradle configuration

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,31 +1,16 @@
-pluginManagement {
+buildscript {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
-    plugins {
-        id("com.android.application") version "8.1.0" apply false
-        id("com.android.library") version "8.1.0" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
 rootProject.name = "luna_tv"
 include(":app")
 
-// Include Flutter Gradle Plugin from Flutter SDK
-val flutterSdkPath = System.getenv("FLUTTER_ROOT") ?: throw GradleException("FLUTTER_ROOT not set")
-includeBuild("$flutterSdkPath/packages/flutter_tools/gradle") {
-    dependencySubstitution {
-        substitute(module("dev.flutter:flutter-gradle-plugin")).using(project(":"))
-    }
-}
+// Flutter configuration
+apply(from: "$System.env.FLUTTER_ROOT/packages/flutter_tools/gradle/app_plugin_loader.gradle")
