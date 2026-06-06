f51da77c5a3d99636e85dbd9ec9114df8a6a939f
djsevenx1 <ci@djsevenx1.dev>
2026-06-06 16:22:21 +0800

    fix: hardcode versionCode/versionName in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "org.moontechlab.lunatv"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     val keystorePropertiesFile = rootProject.file("key.properties")
