357bf1bc5f55ad11bfdbf91ef71c405e29ed8383
DJSeven <djseven@users.noreply.github.com>
2026-06-08 08:24:13 +0800

    fix: downgrade media_kit_video to ^1.2.1 for Flutter 3.22 compat

--- lib/services/downstream_service.dart
+++ lib/services/downstream_service.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:gbk_codec/gbk_codec.dart';
 import 'package:luna_tv/models/search_resource.dart';
 import 'package:luna_tv/models/search_result.dart';
-import 'package:luna_tv/content_filter_service.dart';
-import 'package:luna_tv/local_search_cache_service.dart';
+import 'package:luna_tv/services/content_filter_service.dart';
+import 'package:luna_tv/services/local_search_cache_service.dart';
 
 /// 分页搜索结果
 class SearchPageResult {
